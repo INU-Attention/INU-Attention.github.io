84ac661c37f1d6b09a72201f621aaa912ac5db1b
MintyProtein <dnltjdwls1@naver.com>
2023-10-01 14:17:00 +0900

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-  // ...
-  url: 'https://inu-attention.github.io', // GitHub organization 또는 username + github.io
-  baseUrl: '/inu-attention/', // '/projectName/' 포맷으로 깃헙 레포지토리 이름을 입력한다.
-  projectName: 'inu-attention',
-  organizationName: 'INU-Attention', // GitHub organization 또는 username
-  // ...
+  presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
 };
